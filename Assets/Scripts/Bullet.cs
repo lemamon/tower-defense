5eee82a7bc55c8bd0bb7078128ec47f89705f786
Deyvid Eric de Moraes Marinho <dem@manaus.certi.org>
2018-02-18 23:08:34 -0400

    create tower and bullets

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private float velocity = 10;
+    private GameObject enimy;
+
+    void Start()
+    {
+        enimy = GameObject.Find("Enimy");
+    }
+
+    void Update ()
+    {
+        Move();
+        ChangeDirection();
+	}
+
+    private void Move()
+    {
+        Vector3 bulletPosition = this.transform.position;
+        Vector3 moving = transform.forward * Time.deltaTime * velocity;
+        this.transform.position = bulletPosition + moving;
+    }
+
+    private void ChangeDirection()
+    {
+        Vector3 currentPosition = this.transform.position;
+        Vector3 enimyPosition = enimy.transform.position;
+        Vector3 enimyDirection = enimyPosition - currentPosition;
+
+        this.transform.rotation = Quaternion.LookRotation(enimyDirection);
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+    public GameObject bulletPrefab;
+    private float timeShoot = 1f;
+    private float lastShootTime;
+
+	void Update ()
+    {
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        float currentTime = Time.time;
+        if(currentTime > lastShootTime + timeShoot)
+        {
+            lastShootTime = currentTime;
+            Vector3 shootPoint = this.transform.Find("TowerGun/ShootPoint").gameObject.transform.position;
+            Instantiate(bulletPrefab, shootPoint, Quaternion.identity);
+        }
+      
+    }
+}
