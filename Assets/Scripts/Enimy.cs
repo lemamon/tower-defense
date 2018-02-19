bc03297d033e414d0e9904d7da0f046d78d77cc2
Deyvid Eric de Moraes Marinho <dem@manaus.certi.org>
2018-02-18 20:16:29 -0400

    init project

---
+++ Assets/Scripts/Enimy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enimy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        GameObject endPath = GameObject.Find("EndPath");
+        Vector3 endPathPosition = endPath.transform.position;
+
+        agent.SetDestination(endPathPosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
